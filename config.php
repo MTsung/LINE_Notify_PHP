c27a9a63a504f47887a28a93139191c080444394
MTsung <money015037@gmail.com>
2019-12-06 11:15:33 +0800

    加入github連結

--- config.php
+++ config.php
@@ -10,6 +10,6 @@ define("CLIENT_SECRET","");
 define("CALLBACK_URL","http://localhost/LINE_Notify_PHP/callback.php");
 
 if(!(CLIENT_ID && CLIENT_SECRET && CALLBACK_URL)){
-	exit("請至config.php設定相關參數");
+	exit('請至config.php設定相關參數<br><a href="https://github.com/MTsung/LINE_Notify_PHP" target="_blank">github</a>');
 }
 $lineNotify = new lineNotify(CLIENT_ID,CLIENT_SECRET,CALLBACK_URL);
